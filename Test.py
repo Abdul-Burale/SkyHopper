23283e693b23b0bceee824c69d845cd0d1b409fe
Abdul Burale <abdul.burale1@gmail.com>
2023-03-19 23:28:02 +0000

    Added Terrain SpriteSheet

--- Test.py
+++ Test.py
@@ -1,32 +1,8 @@
-import pygame, sys
+import pygame, sys, csv
 
 if sys.version_info < (3,5):
     print("This game requires at leasst version 3.5 of Python. Please download it from ww.python.org")
     sys.exit()
-
-
-#Define Constants
-WINDOW_WIDTH = 1500
-WINDOW_HEIGHT = 750
-TITLE = "Platformer"
-GRAVITY = 0.5
-
-
-#Initialize Pygame
-pygame.init()
-
-#Window Creation
-WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption(TITLE)
-
-#Game Assets
-BG_IMG = pygame.transform.scale(pygame.image.load("Asset/Background.png"), (WINDOW_WIDTH, WINDOW_HEIGHT)) 
-Sprite_Sheet_Img = pygame.image.load("Asset/SpriteSheet.png")
-
-#Game Settings
-NUM_ROWS, NUM_COLUMNS = 15, 30
-RUN = True
-Clock = pygame.time.Clock()
 
 #Map
 Game_Map_1 = [[0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -43,48 +19,69 @@ Game_Map_1 = [[0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
               [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
               [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
               [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
               [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
 
+#Initialize Pygame
+pygame.init()
+
+#Game Settings
+RUN = True
+Clock = pygame.time.Clock()
+
+#Define Constants
+WINDOW_WIDTH = 1500
+WINDOW_HEIGHT = 750
+TITLE = "Platformer"
+Sprite_Width = 32
+Sprite_Height = 32
+Block_Width = 40
+Block_Height = 40
+NUM_ROWS = 4
+NUM_COLUMNS = 14
+GRAVITY = 0.5
+
+
+
+#Window Creation
+WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption(TITLE)
+
+#Game Assets
+BG_IMG = pygame.transform.scale(pygame.image.load("Asset/Background.png"), (WINDOW_WIDTH, WINDOW_HEIGHT)) 
+Sprite_Sheet_Img = pygame.image.load("Asset/SpriteSheet.png").convert_alpha()
+
+
+
+
 class Level(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.Width = 50
-        self.Height = 50
-        self.Tile_List = []
+    pass
 
-            
+Sprite_List = []
+
+for Row in range(NUM_ROWS):
+    for Col in range(NUM_COLUMNS):
+        x = Col * Block_Width
+        y = Row * Block_Height
+        Sprite_Surface = pygame.Surface((Sprite_Width, Sprite_Height))
+        Sprite_Surface.blit(Sprite_Sheet_Img, (0, 0), (x , y, Sprite_Width, Sprite_Height))
+        Sprite_Rect = pygame.Rect(x, y, Sprite_Width, Sprite_Height)
         
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        #Define Player Attributes
-        self.Width = 50
-        self.Height = 50
-        self.image = ""
-        self.rect = self.image.get_rect()                
-        self.last_update = pygame.time.get_ticks()
-        self.jumped = False
-
-
+        #WINDOW.blit(Sprite_Sheet_Img, (X * 32, Y * 32), (X * 32, Y * 32, 32 , 32))
+        Sprite_List.append((Sprite_Surface, Sprite_Rect))
 
 
 while RUN:
     pygame.display.update()
     WINDOW.blit(BG_IMG, (0,0)) 
-    Row_Count = 0
-    for Row in Game_Map_1:
-        Col_Count = 0
-        for Tile in Row:
-           if Tile == 0:
-            WINDOW.blit(Sprite_Sheet_Img, (Row_Count * 32, Col_Count * 32), (Row_Count * 40, Col_Count * 40, 32 ,32))
-            Col_Count += 1
-    Row_Count += 1
-
-        
     
-    
+    for event in pygame.event.get():
+       pygame.event.get()
+       if event.type == pygame.QUIT:
+           RUN = False
 
-    #WINDOW.blit(Sprite_Sheet_Img, (0,0), (0,40, 32, 32))
+
+    
+    WINDOW.blit(Sprite_List[21][0], (0, 0))
+        #WINDOW.blit(Sprite_Sheet_Img, (0,0), (0,40, 32, 32))
 
     
