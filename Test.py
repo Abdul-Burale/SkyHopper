e4fdcdfae0bbd15b4ca3751d60b4216765dabd37
Abdul Burale <abdul.burale1@gmail.com>
2023-06-02 12:06:24 +0100

    Made a player class, needs quite a bit work still.

--- Test.py
+++ Test.py
@@ -82,6 +82,7 @@ class Platformer:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.RUN = False
+        
 
     def draw(self):
         self.DISPLAY.blit(self.BG_IMG, (0, 0))
@@ -93,14 +94,83 @@ class Platformer:
              #    if 0 <= SPRITE_IDX < len(self.SPRITE_LIST):
                     SPRITE_SURFACE, SPRITE_RECT = self.SPRITE_LIST[SPRITE_IDX]
                     self.DISPLAY.blit(SPRITE_SURFACE, (col * SPRITE_RECT.width, row * SPRITE_RECT.height))
+        Player.update()
+                
+        self.WINDOW.blit(pygame.transform.scale(self.DISPLAY, (self.WINDOW_WIDTH, self.WINDOW_HEIGHT)), (0  , 0))
         
-        self.WINDOW.blit(pygame.transform.scale(self.DISPLAY, (self.WINDOW_WIDTH, self.WINDOW_HEIGHT)), (0 , 0))
+        self.SCROLL[0] -= 0.1
 
-        self.SCROLL[0] += 0.15
+
+class Player:
+    def __init__(self, display):
+        #Player
+        self.DISPLAY = display
+        self.POS_X = 0
+        self.POS_Y = 0
+        self.ACTION = 0
+        self.FRAME = 0
+        self.PLAYER_WIDTH = 50
+        self.PLAYER_HEIGHT = 50
+        self.POS = [self.POS_X, self.POS_Y]
+        self.ACTION_LIST = []
+        self.ACTION_ANI("Asset/idle/man_{}.png", 4)
+        self.ACTION_ANI("Asset/run/man_{}.png", 6)
+
+
+        #Deal with player state animations. (TODO: As gamne gets more complicated come back to update thsi()
+        self.PLAYER_IMAGE = self.ACTION_LIST[self.ACTION][self.FRAME]
+        
+        self.VELOCITY_X = 0
+        self.VELOCITY_Y = 0
+        self.ACCELERATION_X = 0
+        self.ACCELERATION_Y = 0
+        self.GRAVITY = 0.5
+        self.JUMPING = False;
+        self.FALLING = False;
+        self.LAST_UPDATE = pygame.time.get_ticks()
+        #init animations
+
+    def ACTION_ANI(self, PATH, RANGE):
+        Temp_List = []
+        for i in range(RANGE):
+            Image_Path = PATH.format(i)
+            Action_Image = pygame.image.load(Image_Path).convert()
+            Action_Image.set_colorkey((255, 255, 255))
+            Temp_List.append(Action_Image)
+        self.ACTION_LIST.append(Temp_List)
     
+    def update(self):
+        global LAST_UPDATE
+        self.PLAYER_IMAGE = self.ACTION_LIST[self.ACTION][self.FRAME]
+        
+        KEY = pygame.key.get_pressed()
+        if KEY[pygame.K_1]:
+            self.ACTION = 0
+
+        if KEY[pygame.K_2]:
+            self.ACTION = 1
+
+        CURRENT_TIME = pygame.time.get_ticks()
+        if CURRENT_TIME - self.LAST_UPDATE >= 250:
+            self.FRAME += 1
+            self.LAST_UPDATE = CURRENT_TIME
+            if self.ACTION_LIST[0]:
+                if self.FRAME >= 4:
+                    self.FRAME = 0
+
+            elif self.ACTION_LIST[1]:
+                if self.FRAME >= 6:
+                    self.FRAME = 0
+
+        
+
+        self.DISPLAY.blit(self.PLAYER_IMAGE, (self.POS[0], self.POS[1]))
+
 P1 = Platformer()
+Player = Player(P1.DISPLAY)
 while P1.RUN:
     P1.update()
     P1.draw()
+    Player.update()
 
 
